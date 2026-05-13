1115ab1180ae43a47121657331bd1222629f5781
tahzeer <tahzeer.ash@gmail.com>
2026-05-13 16:41:41 +0530

    refactor(data): update SQL data models and templates for farmer extension
    - Added new entries for registry languages and themes in `registry_languages.sql` and `registry_themes.sql`.
    - Updated `incoming_templates.sql` and `outgoing_templates.sql` with new template entries and references to corresponding Jinja2 files.
    - Removed obsolete entries from `g2p_registry_vc_configurations.sql` and added comments for clarity on template file requirements.
    - Enhanced `data_models.sql` with additional comments for template file references.

--- farmer-extension/src/openg2p_registry_farmer_extension/meta_data/registry-outbound-messages-templates/outgoing_templates.sql
+++ farmer-extension/src/openg2p_registry_farmer_extension/meta_data/registry-outbound-messages-templates/outgoing_templates.sql
@@ -1,3 +1,4 @@
-INSERT INTO "public"."outgoing_templates" ("template_id","data_model_id","register_id","template_file_id","created_at","updated_at") VALUES ('689c49df-8a12-407a-8e2a-933c85737a20','c331ba96-ac35-4014-9d13-4ef327f6b79b','a1a4d25a-1cd4-4356-abac-985a0b3c6bcd','833f5e6f7bc04f7188e8684ebd95f364','2026-05-07 04:31:27.828397',NULL);
+INSERT INTO "public"."outgoing_templates" ("template_id","data_model_id","register_id","template_file_id","created_at","updated_at") VALUES
+('OUT-TMPL-1','c331ba96-ac35-4014-9d13-4ef327f6b79b','a1a4d25a-1cd4-4356-abac-985a0b3c6bcd','833f5e6f7bc04f7188e8684ebd95f364','2026-05-07 04:31:27.828397',NULL);
 
--- Template file for template_file_id must be added to MinIO.
+-- OUT-TMPL-1: templates/dci/outgest/openg2p_farmer_to_dci.json.j2
