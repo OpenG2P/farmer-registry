92529eb9ac452186cd511926513d40d7c7b827a4
tahzeer <tahzeer.ash@gmail.com>
2026-05-26 14:52:08 +0530

    G2P-4932 feat(data): update SQL data models and templates for farmer extension
    - modified SQL insert statements for data models
    - re-structure templates dir for incoming and outgoing messages

--- farmer-extension/src/openg2p_registry_farmer_extension/meta_data/register-metadata/g2p_registry_documents.sql
+++ farmer-extension/src/openg2p_registry_farmer_extension/meta_data/register-metadata/g2p_registry_documents.sql
@@ -1,5 +1,4 @@
 INSERT INTO "public"."g2p_registry_documents" ("document_id","document_store_id","document_label","filename") VALUES 
-('1f0953d4-f0fb-4336-a126-4d0519a74ffc','d9c734dd3b3044e49d422eb6cbbace40','jinja-template','dci_commons_response.json.j2'),
-('1f997c2f-f768-4224-96a9-f60938224dda','833f5e6f7bc04f7188e8684ebd95f364','jinja-template','dci_search_openg2p_farmer_to_dci.json.j2');
-
--- Template binaries referenced by document_store_id / filename must be added to MinIO.
+('1f0953d4-f0fb-4336-a126-4d0519a74ffc','dci_commons_response.json.j2','jinja-template','dci_commons_response.json.j2'),
+('1f997c2f-f768-4224-96a9-f60938224dda','dci_to_openg2p_farmer.json.j2','jinja-template','dci_to_openg2p_farmer.json.j2'),
+('2a8b3c4d-5e6f-7890-abcd-ef1234567890','openg2p_farmer_to_dci.json.j2','jinja-template','openg2p_farmer_to_dci.json.j2');
--- farmer-extension/src/openg2p_registry_farmer_extension/meta_data/registry-inbound-message-rules/incoming_templates.sql
+++ farmer-extension/src/openg2p_registry_farmer_extension/meta_data/registry-inbound-message-rules/incoming_templates.sql
@@ -1,3 +1,3 @@
-INSERT INTO "public"."incoming_templates" ("template_id","data_model_id","register_id","template_file_id","created_at","updated_at") VALUES ('IN-TMPL-1','c331ba96-ac35-4014-9d13-4ef327f6b79b','a1a4d25a-1cd4-4356-abac-985a0b3c6bcd','833f5e6f7bc04f7188e8684ebd95f364','2026-05-07 04:31:27.828397',NULL);
+INSERT INTO "public"."incoming_templates" ("template_id","data_model_id","register_id","template_file_id","created_at","updated_at") VALUES ('IN-TMPL-1','c331ba96-ac35-4014-9d13-4ef327f6b79b','a1a4d25a-1cd4-4356-abac-985a0b3c6bcd','dci_to_openg2p_farmer.json.j2','2026-05-07 04:31:27.828397',NULL);
 
--- IN-TMPL-1: templates/dci/ingest/dci_search_openg2p_farmer_to_dci.json.j2
+-- IN-TMPL-1: dci_to_openg2p_farmer.json.j2
